d8f83ad44d62e95653c7b52ef470b5b2b73252df
Benjamin Park <baboring@gmail.com>
2017-06-28 14:03:13 -0400

    Image Async Loader

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,7 @@
 angular.module('starter.services', [])
-.factory('UserService',[ '$q', '$ionicLoading', '$ionicPopup',
-function( $q, $ionicLoading, $ionicPopup){
+
+.factory('UserService',[ '$q', '$ionicLoading', '$ionicPopup'
+,function( $q, $ionicLoading, $ionicPopup) {
     var ref = firebase.database().ref('/');
 
     var self = {
@@ -75,4 +76,190 @@ function( $q, $ionicLoading, $ionicPopup){
         });                
     };
     return self;
-}]);
+}])
+
+.factory('WeeklyService',[ '$q', '$ionicLoading', '$ionicPopup'
+,function( $q, $ionicLoading, $ionicPopup) {
+    var ref = firebase.database().ref('/');
+
+    var self = {
+        all: {},
+        upload: upload
+    }
+
+    getAll();
+
+
+    function getAll() {
+        ref.child('weekly')
+        .on('child_added',function(snap){
+            self.all[snap.key] = snap.val();
+        });
+    }
+
+    function upload() {
+
+    }
+}])
+
+
+.factory('PhotoService',[ '$log','$q','Firebase', '$cordovaCamera','$ionicLoading'
+,function PhotoService ($log, $q, Firebase, $cordovaCamera, $ionicLoading) {
+        var self = {};
+
+        angular.extend(self,{
+            images: {
+                data: {},
+                get: getImage,
+            },
+            takeImage: takeImage,
+            UpdateImageFromBase64: UpdateImageFromBase64,
+        });
+
+        // load profile image from server
+        var ref = Firebase.storage().ref('/');
+
+        function UpdateImageFromBase64(imageBase64) {
+            var deferred = $q.defer();
+            self.savePhoto('data:image/png;base64,'+imageBase64);
+            UploadImageBase64(imageBase64)
+                .then(function (downloadURL) {
+                    deferred.resolve(downloadURL);
+                });
+            return deferred.promise;
+        }
+        
+        function UploadImage(refPath,file) {
+            var deferred = $q.defer();
+            if(UserService.getProfile()) {
+                var task = ref.child('images/'+refPath)
+                .put(file);
+
+                UploadTaskProgress(task).then(function(downloadURL){
+                    deferred.resolve(downloadURL);
+                });
+            }
+            return deferred.promise;
+        }
+
+        function UploadImageBase64(refPath,imageBase64) {
+            var deferred = $q.defer();
+            if(UserService.getProfile()) {
+                var task = ref.child('images/'+refPath)
+                .putString(imageBase64,'base64', {contentType:'image/jpg'});
+
+                UploadTaskProgress(task).then(function(downloadURL){
+                    deferred.resolve(downloadURL);
+                });
+            }
+            return deferred.promise;
+        }
+
+        // Upload file
+        function UploadTaskProgress(uploadTask) {
+            var deferred = $q.defer();
+            $ionicLoading.show('uploading..');
+
+            uploadTask.on('state_changed', function(snapshot){
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            $log.debug('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    $log.debug('Upload is paused');
+                break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    $log.debug('Upload is running');
+                break;
+            }
+            }, function(error) {
+                // Handle unsuccessful uploads
+            }, function() {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                deferred.resolve(uploadTask.snapshot.downloadURL);
+                $ionicLoading.hide();
+            });             
+          
+            return deferred.promise;
+        }
+
+        function takeImage() {
+            var deferred = $q.defer();
+            var options = {
+                quality: 80,
+                destinationType: Camera.DestinationType.DATA_URL,
+                // sourceType: Camera.PictureSourceType.CAMERA,
+                sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 250,
+                targetHeight: 250,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
+            
+            $cordovaCamera.getPicture(options).then(function(base64ImageData) {
+                //$log.debug('getPicture : '+ base64ImageData);
+                deferred.resolve(base64ImageData);
+            }, function(err) {
+                // error
+                deferred.resolve(error);
+            });
+            return deferred.promise;
+        }
+
+        function findImage(uuid) {
+            for(var obj in self.images.data)
+                if(self.images.data[obj] && self.images.data[obj].uuid == uuid)
+                    return self.images.data[obj];
+            return null;
+        }
+        function getImage(imagePath) {
+            var found = findImage(imagePath);
+            if(!found) {
+                found = {
+                    imagePath: imagePath,
+                    url: null,
+                    error: 'object_not_found',
+                }
+                self.images.data[imagePath] = found;
+            }            
+            if(uuid && !found.url && found.error) {
+                found.error = null;
+                ref.child('images/'+imagePath)
+                .getDownloadURL().then(function(url){
+                    $log.debug('imagePath',url);
+                    found.url = url;
+                    EventTrigger.event('loaded-url');
+                })
+                .catch(function(error) {
+                    found.error = error.code;
+                    $log.error(error.code,error);
+                    switch (error.code) {
+                        case 'storage/object_not_found':
+                        // File doesn't exist
+                        break;
+
+                        case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+
+                        case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+
+                        case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                    }
+                });
+            }
+            return found;
+
+        }
+        
+        return self;        
+}])
+;
