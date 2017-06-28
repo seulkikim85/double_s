7db461759f18dd9aeb73e561f195923601be6b12
Benjamin Park <baboring@gmail.com>
2017-06-28 15:22:49 -0400

    Example Submit Article

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -243,7 +243,7 @@ angular.module('starter.controllers', [])
      }
     };
 })
-.controller('matchingCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, $ionicLoading) {
+.controller('matchingCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, $ionicLoading, $rootScope) {
     var storage = firebase.storage();
 
     
@@ -252,15 +252,50 @@ angular.module('starter.controllers', [])
         $scope.bgImg = url;
         console.log(url);
     });
+
     // Activate ink for controller
     ionicMaterialInk.displayEffect();
 
-    var vm = $scope.vm = {};
-    vm.imgSrc1 = 'https://firebasestorage.googleapis.com/v0/b/capstone-project-a56d3.appspot.com/o/bag.jpg?alt=media&token=ab5039c3-3aeb-451a-9d3d-b12d379f99fa';
-    vm.imgSrc2 = 'https://firebasestorage.googleapis.com/v0/b/capstone-project-a56d3.appspot.com/o/hat.jpg?alt=media&token=ab768762-f75e-4a47-a269-fb6377342860';
+    var vm = $scope.vm = {
+        list: []
+    };
+
+    // sync list
+    var ref = firebase.database().ref('/');
+    ref.child("matching").on('child_added', function (snapshot) {
+        var info = snapshot.val();
+        vm.list.push(info);
+    });
+
     $scope.UploadContent = function() {
         console.log('UploadContent');
-        document.getElementById("idFile").click();
+        //document.getElementById("idFile").click();
+
+        if(!$rootScope.currentUser) {
+            alert('Need Login');
+            return;
+        }
+
+        var newContent ={
+                owner: $rootScope.currentUser.uid,
+                writter: $rootScope.currentUser.email,
+                title: 'new title',
+                caption: 'new Caption',
+                img1: 'https://firebasestorage.googleapis.com/v0/b/capstone-project-a56d3.appspot.com/o/style-dress%2Fhat.jpg?alt=media&token=5814b47a-8b97-4728-9ccf-7b47b51a0e2d',
+                img2: 'https://firebasestorage.googleapis.com/v0/b/capstone-project-a56d3.appspot.com/o/style-dress%2Fbag.jpg?alt=media&token=d917fa38-6ffe-46d3-a5e5-bc94bbb16c73',
+                like2: 0,
+                comments: {
+                    uuid: '123123',
+                    writter: 'writter name',
+                    content: 'weefefefefef efefef',
+                    timestamp: Date.now() 
+                },
+                timestamp: Date.now() 
+            };
+
+        var ref = firebase.database().ref('/');
+        ref.child("matching").push().set(newContent);
+        
     }
 
     $scope.fileSelect = function (files) {
