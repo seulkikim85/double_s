389ca2f1530307cde5c74a7f96fb046e1cf0e808
Benjamin Park <baboring@gmail.com>
2017-07-05 13:53:52 -0400

    Bug Fix - Watching view

--- www/js/controllers/matchingCtrl.js
+++ www/js/controllers/matchingCtrl.js
@@ -1,21 +1,19 @@
-ctrlModule.controller('matchingCtrl', ['$scope', '$state', '$rootScope','$stateParams', 
-    '$timeout', 'ionicMaterialInk', 'ionicMaterialMotion' ,'$ionicModal',
-    '$ionicLoading','$ionicPopup','MatchService','PhotoService','Tools','EventTrigger'
+ctrlModule.controller('matchingCtrl', ['$scope','MatchService','EventTrigger','$ionicModal'
+,'$rootScope','PhotoService','$ionicLoading','$ionicPopup'
+, function($scope,MatchService,EventTrigger,$ionicModal,$rootScope
+, PhotoService,$ionicLoading,$ionicPopup ) {
 
-, function($scope, $state, $rootScope, $stateParams, $timeout, ionicMaterialInk, $ionicMaterialMotion, $ionicModal,$ionicLoading,$ionicPopup, WeeklyService, PhotoService, Tools,EventTrigger)  {
+    // $scope.$parent.showHeader();
+    // $scope.$parent.clearFabs();
+    // $scope.isExpanded = true;
+    // $scope.$parent.setExpanded(true);
+    // $scope.$parent.setHeaderFab(false);
 
-
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = true;
-    $scope.$parent.setExpanded(true);
-    $scope.$parent.setHeaderFab(false);
-
-    ionicMaterialInk.displayEffect();
+    // ionicMaterialInk.displayEffect();
 
     var storage = firebase.storage();
 
-     var vm = $scope.vm = {
+    var vm = $scope.vm = {
         list_left: MatchService.list_left,
         list_right: MatchService.list_right,
         previewer: null,
@@ -39,8 +37,8 @@ ctrlModule.controller('matchingCtrl', ['$scope', '$state', '$rootScope','$stateP
             timestamp: firebase.database.ServerValue.TIMESTAMP
         },
         base64img: null,
-        Submit: function() {
-            if(!vm.base64img) {
+        Submit: function () {
+            if (!vm.base64img) {
                 $ionicPopup.alert({
                     title: 'Fails',
                     template: 'No Image!'
@@ -48,32 +46,32 @@ ctrlModule.controller('matchingCtrl', ['$scope', '$state', '$rootScope','$stateP
                 return;
             }
             MatchService.upload(vm.base64img, vm.form)
-            .then(function() {
-                vm.modal.hide();
-            });
+                .then(function () {
+                    vm.modal.hide();
+                });
         }
     }
-      EventTrigger.add('loaded-url-matching',function(){
-          if(!EventTrigger.isRefreshing())
-              $scope.$apply();
-      });   
+    EventTrigger.add('loaded-url-matching', function () {
+        if (!EventTrigger.isRefreshing())
+            $scope.$apply();
+    });
 
-     //-----------------------------------------------------
+    // //-----------------------------------------------------
 
-      $ionicModal.fromTemplateUrl('templates/photoUpload.html', {
+    $ionicModal.fromTemplateUrl('templates/photoUpload.html', {
         controller: 'photoUploadCtrl',
         scope: $scope,
         animation: 'slide-in-up'
-    }).then(function(modal) {
+    }).then(function (modal) {
         vm.modal = modal;
     });
-    
-    $scope.closeModal = function() {
+
+    $scope.closeModal = function () {
         vm.modal.hide();
     }
-    $scope.UploadContent = function() {
+    $scope.UploadContent = function () {
         console.log('UploadContent');
-        if(!$rootScope.currentUser) {
+        if (!$rootScope.currentUser) {
             $ionicPopup.alert({
                 title: 'Required Authentication!!',
                 template: 'Log in Please!!'
@@ -93,29 +91,29 @@ ctrlModule.controller('matchingCtrl', ['$scope', '$state', '$rootScope','$stateP
         vm.previewer = document.getElementById('previewer');
         vm.previewer.src = "img/upload_bg.jpg";
     }
-    vm.selectPhoto = function() {
+    vm.selectPhoto = function () {
         console.log('select Photo');
         document.getElementById("idFile").click();
     }
 
     $scope.fileSelect = function (files) {
         vm.file = files[0];
-      
+
         $ionicLoading.show('image processing..')
         PhotoService.LoadOrientationImage(vm.file, function (base64img, value) {
-            if(value == 1) {
+            if (value == 1) {
                 vm.previewer.src = base64img;
                 vm.base64img = base64img;
                 $ionicLoading.hide();
             }
             else {
-                Tools.resetOrientation(base64img,value,function(resetBase64Img){
+                Tools.resetOrientation(base64img, value, function (resetBase64Img) {
                     vm.previewer.src = resetBase64Img;
                     vm.base64img = resetBase64Img;
                     $ionicLoading.hide();
                 });
             }
-        });     
+        });
 
     }
 
