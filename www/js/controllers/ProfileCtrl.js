2d11b81e42e96e0e0d8e1be7f3ead32376460628
Benjamin Park <baboring@gmail.com>
2017-07-17 19:21:33 -0400

    back button display working

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,6 +1,7 @@
 
 ctrlModule.controller('ProfileCtrl', function($scope, $rootScope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk
-,$ionicPopup ,$ionicLoading, PhotoService, UserService  ) {
+,$ionicPopup ,$ionicLoading, PhotoService, UserService, $ionicHistory  ) {
+    
     // Set Ink
     ionicMaterialInk.displayEffect();
     var vm = $scope.vm = {
@@ -11,6 +12,8 @@ ctrlModule.controller('ProfileCtrl', function($scope, $rootScope, $stateParams, 
         newPassword: '',
         myAvatarImg: null
     };
+    vm.user = $rootScope.currentUser;
+    
     // Set Motion
     $timeout(function() {
         ionicMaterialMotion.slideUp({
@@ -21,14 +24,24 @@ ctrlModule.controller('ProfileCtrl', function($scope, $rootScope, $stateParams, 
     vm.isLogined = function() {
         return $rootScope.currentUser != undefined;
     }    
-    vm.user = $rootScope.currentUser;
 
-    $scope.$on('$ionicView.enter', function(e) {
+    $scope.$on('$ionicView.enter', function(e,v,a) {
         vm.myAvatarImg = UserService.loadMyAvatar();
         vm.user = $rootScope.currentUser;
         if(!$rootScope.$root.$$phase)
             $scope.$apply();
     });
+    // Change menu button to back button for going back
+    // $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
+    //     if($rootScope.previousState)
+    //         viewData.enableBack = true;        
+    // });
+    $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
+        var histroyBack = $ionicHistory.backView();
+        if(histroyBack)
+            viewData.enableBack = true;        
+    });
+    
     console.log('user',$rootScope.currentUser);
 
     vm.UploadContent = function() {
